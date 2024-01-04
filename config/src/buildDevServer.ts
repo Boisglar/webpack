256a31ba23c6872585b9e249bc2a0d29ad493f61
Baysangur Delbiev <111337500+Boisglar@users.noreply.github.com>
2024-01-04 03:51:40 +0300

    исправил ошибку с оператором &&

--- config/src/buildDevServer.ts
+++ config/src/buildDevServer.ts
@@ -4,7 +4,7 @@ import { BuildOptions } from "./types/types";
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
-        port: options.port && 3000,
+        port: options.port ?? 3000,
         open: true,
         // нужно для того чтобы наш сервер понимал что это приложение SPA 
         historyApiFallback: true,
